942cf00bbf3ad531d0d45b6a773bac7c30ea2531
MFaiqKhan <faiqkhanthinkcentre@yahoo.com>
2022-11-08 23:14:35 +0500

    chore: update config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,17 +1,19 @@
-import "@nomicfoundation/hardhat-toolbox";
+/* import "@nomicfoundation/hardhat-toolbox";
 import { config as dotenvConfig } from "dotenv";
 import type { HardhatUserConfig } from "hardhat/config";
 import type { NetworkUserConfig } from "hardhat/types";
 import { resolve } from "path";
 
 import "./tasks/accounts";
-import "./tasks/deploy";
+import "./tasks/deploy/populatedescriptor";
 
 const dotenvConfigPath: string = process.env.DOTENV_CONFIG_PATH || "./.env";
 dotenvConfig({ path: resolve(__dirname, dotenvConfigPath) });
 
 // Ensure that we have all the environment variables we need.
 const mnemonic: string | undefined = process.env.MNEMONIC;
+const WALLET_PRIVATE_KEY: string | undefined = process.env.WALLET_PRIVATE_KEY;
+
 if (!mnemonic) {
   throw new Error("Please set your MNEMONIC in a .env file");
 }
@@ -48,7 +50,7 @@ function getChainConfig(chain: keyof typeof chainIds): NetworkUserConfig {
   return {
     accounts: {
       count: 10,
-      mnemonic,
+      WALLET_PRIVATE_KEY,
       path: "m/44'/60'/0'/0",
     },
     chainId: chainIds[chain],
@@ -79,14 +81,18 @@ const config: HardhatUserConfig = {
   networks: {
     hardhat: {
       accounts: {
-        mnemonic,
+        //[WALLET_PRIVATE_KEY],
       },
-      chainId: chainIds.hardhat,
+      chainId: chainIds.goerli,
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [WALLET_PRIVATE_KEY!].filter(Boolean),
     },
     arbitrum: getChainConfig("arbitrum-mainnet"),
     avalanche: getChainConfig("avalanche"),
     bsc: getChainConfig("bsc"),
-    goerli: getChainConfig("goerli"),
+    //goerli: getChainConfig("goerli"),
     mainnet: getChainConfig("mainnet"),
     optimism: getChainConfig("optimism-mainnet"),
     "polygon-mainnet": getChainConfig("polygon-mainnet"),
@@ -120,4 +126,335 @@ const config: HardhatUserConfig = {
   },
 };
 
+export default config;
+ */
+
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-etherscan";
+import "@typechain/hardhat";
+import dotenv from "dotenv";
+import "hardhat-gas-reporter";
+import { HardhatUserConfig } from "hardhat/config";
+import "solidity-coverage";
+
+import "./tasks/deploy/populatedescriptor";
+
+/* import "@nomicfoundation/hardhat-toolbox";
+import { config as dotenvConfig } from "dotenv";
+import type { HardhatUserConfig } from "hardhat/config";
+import type { NetworkUserConfig } from "hardhat/types";
+import { resolve } from "path";
+
+import "./tasks/accounts";
+
+
+const dotenvConfigPath: string = process.env.DOTENV_CONFIG_PATH || "./.env";
+dotenvConfig({ path: resolve(__dirname, dotenvConfigPath) });
+
+// Ensure that we have all the environment variables we need.
+const mnemonic: string | undefined = process.env.MNEMONIC;
+const WALLET_PRIVATE_KEY: string | undefined = process.env.WALLET_PRIVATE_KEY;
+
+if (!mnemonic) {
+  throw new Error("Please set your MNEMONIC in a .env file");
+}
+
+const infuraApiKey: string | undefined = process.env.INFURA_API_KEY;
+if (!infuraApiKey) {
+  throw new Error("Please set your INFURA_API_KEY in a .env file");
+}
+
+const chainIds = {
+  "arbitrum-mainnet": 42161,
+  avalanche: 43114,
+  bsc: 56,
+  goerli: 5,
+  hardhat: 31337,
+  mainnet: 1,
+  "optimism-mainnet": 10,
+  "polygon-mainnet": 137,
+  "polygon-mumbai": 80001,
+};
+
+function getChainConfig(chain: keyof typeof chainIds): NetworkUserConfig {
+  let jsonRpcUrl: string;
+  switch (chain) {
+    case "avalanche":
+      jsonRpcUrl = "https://api.avax.network/ext/bc/C/rpc";
+      break;
+    case "bsc":
+      jsonRpcUrl = "https://bsc-dataseed1.binance.org";
+      break;
+    default:
+      jsonRpcUrl = "https://" + chain + ".infura.io/v3/" + infuraApiKey;
+  }
+  return {
+    accounts: {
+      count: 10,
+      WALLET_PRIVATE_KEY,
+      path: "m/44'/60'/0'/0",
+    },
+    chainId: chainIds[chain],
+    url: jsonRpcUrl,
+  };
+}
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  etherscan: {
+    apiKey: {
+      arbitrumOne: process.env.ARBISCAN_API_KEY || "",
+      avalanche: process.env.SNOWTRACE_API_KEY || "",
+      bsc: process.env.BSCSCAN_API_KEY || "",
+      goerli: process.env.ETHERSCAN_API_KEY || "",
+      mainnet: process.env.ETHERSCAN_API_KEY || "",
+      optimisticEthereum: process.env.OPTIMISM_API_KEY || "",
+      polygon: process.env.POLYGONSCAN_API_KEY || "",
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY || "",
+    },
+  },
+  gasReporter: {
+    currency: "USD",
+    enabled: process.env.REPORT_GAS ? true : false,
+    excludeContracts: [],
+    src: "./contracts",
+  },
+  networks: {
+    hardhat: {
+      accounts: {
+        //[WALLET_PRIVATE_KEY],
+      },
+      chainId: chainIds.goerli,
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [WALLET_PRIVATE_KEY!].filter(Boolean),
+    },
+    arbitrum: getChainConfig("arbitrum-mainnet"),
+    avalanche: getChainConfig("avalanche"),
+    bsc: getChainConfig("bsc"),
+    //goerli: getChainConfig("goerli"),
+    mainnet: getChainConfig("mainnet"),
+    optimism: getChainConfig("optimism-mainnet"),
+    "polygon-mainnet": getChainConfig("polygon-mainnet"),
+    "polygon-mumbai": getChainConfig("polygon-mumbai"),
+  },
+  paths: {
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./contracts",
+    tests: "./test",
+  },
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      metadata: {
+        // Not including the metadata hash
+        // https://github.com/paulrberg/hardhat-template/issues/31
+        bytecodeHash: "none",
+      },
+      // Disable the optimizer when debugging
+      // https://hardhat.org/hardhat-network/#solidity-optimizer-support
+      optimizer: {
+        enabled: true,
+        runs: 800,
+      },
+    },
+  },
+  typechain: {
+    outDir: "types",
+    target: "ethers-v5",
+  },
+};
+
+export default config;
+ */
+import "./tasks/deploy/populatedescriptor";
+
+/* import "@nomicfoundation/hardhat-toolbox";
+import { config as dotenvConfig } from "dotenv";
+import type { HardhatUserConfig } from "hardhat/config";
+import type { NetworkUserConfig } from "hardhat/types";
+import { resolve } from "path";
+
+import "./tasks/accounts";
+
+
+const dotenvConfigPath: string = process.env.DOTENV_CONFIG_PATH || "./.env";
+dotenvConfig({ path: resolve(__dirname, dotenvConfigPath) });
+
+// Ensure that we have all the environment variables we need.
+const mnemonic: string | undefined = process.env.MNEMONIC;
+const WALLET_PRIVATE_KEY: string | undefined = process.env.WALLET_PRIVATE_KEY;
+
+if (!mnemonic) {
+  throw new Error("Please set your MNEMONIC in a .env file");
+}
+
+const infuraApiKey: string | undefined = process.env.INFURA_API_KEY;
+if (!infuraApiKey) {
+  throw new Error("Please set your INFURA_API_KEY in a .env file");
+}
+
+const chainIds = {
+  "arbitrum-mainnet": 42161,
+  avalanche: 43114,
+  bsc: 56,
+  goerli: 5,
+  hardhat: 31337,
+  mainnet: 1,
+  "optimism-mainnet": 10,
+  "polygon-mainnet": 137,
+  "polygon-mumbai": 80001,
+};
+
+function getChainConfig(chain: keyof typeof chainIds): NetworkUserConfig {
+  let jsonRpcUrl: string;
+  switch (chain) {
+    case "avalanche":
+      jsonRpcUrl = "https://api.avax.network/ext/bc/C/rpc";
+      break;
+    case "bsc":
+      jsonRpcUrl = "https://bsc-dataseed1.binance.org";
+      break;
+    default:
+      jsonRpcUrl = "https://" + chain + ".infura.io/v3/" + infuraApiKey;
+  }
+  return {
+    accounts: {
+      count: 10,
+      WALLET_PRIVATE_KEY,
+      path: "m/44'/60'/0'/0",
+    },
+    chainId: chainIds[chain],
+    url: jsonRpcUrl,
+  };
+}
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  etherscan: {
+    apiKey: {
+      arbitrumOne: process.env.ARBISCAN_API_KEY || "",
+      avalanche: process.env.SNOWTRACE_API_KEY || "",
+      bsc: process.env.BSCSCAN_API_KEY || "",
+      goerli: process.env.ETHERSCAN_API_KEY || "",
+      mainnet: process.env.ETHERSCAN_API_KEY || "",
+      optimisticEthereum: process.env.OPTIMISM_API_KEY || "",
+      polygon: process.env.POLYGONSCAN_API_KEY || "",
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY || "",
+    },
+  },
+  gasReporter: {
+    currency: "USD",
+    enabled: process.env.REPORT_GAS ? true : false,
+    excludeContracts: [],
+    src: "./contracts",
+  },
+  networks: {
+    hardhat: {
+      accounts: {
+        //[WALLET_PRIVATE_KEY],
+      },
+      chainId: chainIds.goerli,
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [WALLET_PRIVATE_KEY!].filter(Boolean),
+    },
+    arbitrum: getChainConfig("arbitrum-mainnet"),
+    avalanche: getChainConfig("avalanche"),
+    bsc: getChainConfig("bsc"),
+    //goerli: getChainConfig("goerli"),
+    mainnet: getChainConfig("mainnet"),
+    optimism: getChainConfig("optimism-mainnet"),
+    "polygon-mainnet": getChainConfig("polygon-mainnet"),
+    "polygon-mumbai": getChainConfig("polygon-mumbai"),
+  },
+  paths: {
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./contracts",
+    tests: "./test",
+  },
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      metadata: {
+        // Not including the metadata hash
+        // https://github.com/paulrberg/hardhat-template/issues/31
+        bytecodeHash: "none",
+      },
+      // Disable the optimizer when debugging
+      // https://hardhat.org/hardhat-network/#solidity-optimizer-support
+      optimizer: {
+        enabled: true,
+        runs: 800,
+      },
+    },
+  },
+  typechain: {
+    outDir: "types",
+    target: "ethers-v5",
+  },
+};
+
+export default config;
+ */
+
+import "./tasks/deploy/populatedescriptor";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.15",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 10_000,
+      },
+    },
+  },
+  networks: {
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.WALLET_PRIVATE_KEY!].filter(Boolean),
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: process.env.MNEMONIC
+        ? { mnemonic: process.env.MNEMONIC }
+        : [process.env.WALLET_PRIVATE_KEY!].filter(Boolean),
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.WALLET_PRIVATE_KEY!].filter(Boolean),
+    },
+    hardhat: {
+      initialBaseFeePerGas: 0,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  /*  abiExporter: {
+    path: "./abi",
+    clear: true,
+  }, */
+  typechain: {
+    outDir: "./typechain",
+  },
+  gasReporter: {
+    enabled: !process.env.CI,
+    currency: "USD",
+    gasPrice: 50,
+    src: "contracts",
+    coinmarketcap: "7643dfc7-a58f-46af-8314-2db32bdd18ba",
+  },
+  mocha: {
+    timeout: 60_000,
+  },
+};
 export default config;
